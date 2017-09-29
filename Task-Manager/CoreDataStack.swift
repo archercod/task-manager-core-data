99a3e4c78c70d5bc821a947dbb9734d3775401a9
marcinoc <pietrzakmarcin@hotmail.com>
2017-09-29 23:42:20 +0200

    add managed object model

--- Task-Manager/CoreDataStack.swift
+++ Task-Manager/CoreDataStack.swift
@@ -7,13 +7,19 @@
 //
 
 import Foundation
+import CoreData
 
 class CoreDataStack {
     
     // Create place to store data
-    lazy var applicationDocumentsDirectory: URL = {
+    private(set) lazy var applicationDocumentsDirectory: URL = {
         let urls = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
         let endIndex = urls.index(before: urls.endIndex)
         return urls[endIndex]
+    }()
+    
+    private(set) lazy var managedObjectModel: NSManagedObjectModel = {
+        let modelUrl = Bundle.main.url(forResource: "Task-Manager", withExtension: "momd")!
+        return NSManagedObjectModel(contentsOf: modelUrl)!
     }()
 }
