95c704f378f1d8b0c1d1291e8e1a32c4d5f6170a
marcinoc <pietrzakmarcin@hotmail.com>
2017-10-01 21:57:51 +0200

    Create User Interface

---
+++ Task-Manager/NewTaskController.swift
@@ -0,0 +1,29 @@
+//
+//  NewTaskController.swift
+//  Task-Manager
+//
+//  Created by Marcin Pietrzak on 01.10.2017.
+//  Copyright © 2017 Marcin Pietrzak. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
