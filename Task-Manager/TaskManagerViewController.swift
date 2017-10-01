02976e4ff2f6091105965bf03a29a8f708305e76
marcinoc <pietrzakmarcin@hotmail.com>
2017-10-01 22:49:14 +0200

    refactoring TaskManager using fetched results controller

--- Task-Manager/TaskManagerViewController.swift
+++ Task-Manager/TaskManagerViewController.swift
@@ -12,19 +12,18 @@ import CoreData
 class TaskManagerViewController: UITableViewController {
     
     let managedObjectContext = CoreDataStack().managedObjectContext
-    var items = [Item]() {
-        didSet {
-            tableView.reloadData()
-        }
-    }
+    
+    lazy var fetchedResultsController: NSFetchedResultsController<Item> = {
+        let request: NSFetchRequest<Item> = Item.fetchRequest()
+        let controller = NSFetchedResultsController(fetchRequest: request, managedObjectContext: self.managedObjectContext, sectionNameKeyPath: nil, cacheName: nil)
+        return controller
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
         
         do {
-            items = try managedObjectContext.fetch(request)
+            try fetchedResultsController.performFetch()
         }
         catch {
             print("Error fetching Item objects: \(error.localizedDescription)")
@@ -34,20 +33,24 @@ class TaskManagerViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return fetchedResultsController.sections?.count ?? 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return items.count
+        guard let section = fetchedResultsController.sections?[section] else { return 0 }
+        return section.numberOfObjects
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
 
-        let item = items[indexPath.row]
+        return configureCell(cell, at: indexPath)
+    }
+    
+    private func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
+        let item = fetchedResultsController.object(at: indexPath)
         cell.textLabel?.text = item.text
-        
         return cell
     }
     
