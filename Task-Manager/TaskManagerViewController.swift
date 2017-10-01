e037439616bed7af55042c32029fa8b8d873bcc2
marcinoc <pietrzakmarcin@hotmail.com>
2017-10-01 23:21:13 +0200

    add swipe to delete row functionality in task manager

--- Task-Manager/TaskManagerViewController.swift
+++ Task-Manager/TaskManagerViewController.swift
@@ -40,10 +40,21 @@ class TaskManagerViewController: UITableViewController {
         return configureCell(cell, at: indexPath)
     }
     
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        let item = fetchedResultsController.object(at: indexPath)
+        managedObjectContext.delete(item)
+        managedObjectContext.saveChanges()
+    }
+    
     private func configureCell(_ cell: UITableViewCell, at indexPath: IndexPath) -> UITableViewCell {
         let item = fetchedResultsController.object(at: indexPath)
         cell.textLabel?.text = item.text
         return cell
+    }
+    
+    // MARK: UITableViewDelegate
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .delete
     }
     
     // MARK: Navigation
